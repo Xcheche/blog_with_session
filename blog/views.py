3295d3cd09b3039323f8848b6ac9b830f7c858d0
xcheche <chekwubechukwu23@gmail.com>
2025-02-15 12:06:15 +0100

    base.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 # Create your views here.
+
+
+def post_page(request,slug):
+    post = Post.objects.get(slug=slug)
+    
+    
+    context = {
+        'post': post,
+    }
+    return render(request,'blog/post.html',context)
